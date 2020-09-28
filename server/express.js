f13e4e40328d11c1bfe9c2a8fbb50c37bb3da1e5
Tarun yadav <56935638+tarunyadav1@users.noreply.github.com>
2020-09-28 23:07:35 +0530

    client added

--- server/express.js
+++ server/express.js
@@ -4,25 +4,63 @@ import cookieParser from "cookie-parser";
 import compress from "compression";
 import cors from "cors";
 import helmet from "helmet";
+import path from "path";
 import Template from "./../template";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 
+// modules for server side rendering
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import MainRouter from "./../client/MainRouter";
+import { StaticRouter } from "react-router-dom";
+import { ServerStyleSheets, ThemeProvider } from "@material-ui/styles";
+import theme from "./../client/theme";
+// end
+
+//comment out before building for production
+import devBundle from "./devBundle";
+
 const app = express();
+
+devBundle.compile(app);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.status(200).send(Template());
-});
-
-app.use("/", userRoutes);
-app.use("/", authRoutes);
-
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+
+// mount routes
+app.use("/", userRoutes);
+app.use("/", authRoutes);
+
+app.get("*", (req, res) => {
+  const sheets = new ServerStyleSheets();
+  const context = {};
+  const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+      <StaticRouter location={req.url} context={context}>
+        <ThemeProvider theme={theme}>
+          <MainRouter />
+        </ThemeProvider>
+      </StaticRouter>
+    )
+  );
+  if (context.url) {
+    return res.redirect(303, context.url);
+  }
+  const css = sheets.toString();
+  res.status(200).send(
+    Template({
+      markup: markup,
+      css: css,
+    })
+  );
+});
 
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -1,22 +1,18 @@
 const path = require("path");
-const nodeExternals = require("webpack-node-externals");
 const CURRENT_WORKING_DIR = process.cwd();
 
 const config = {
-  name: "server",
-  entry: [path.join(CURRENT_WORKING_DIR, "./server/server.js")],
-  target: "node",
+  mode: "production",
+  entry: [path.join(CURRENT_WORKING_DIR, "client/main.js")],
   output: {
-    path: path.join(CURRENT_WORKING_DIR, "/dist/"),
-    filename: "server.generated.js",
+    path: path.join(CURRENT_WORKING_DIR, "/dist"),
+    filename: "bundle.js",
     publicPath: "/dist/",
-    libraryTarget: "commonjs2",
   },
-  externals: [nodeExternals()],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
